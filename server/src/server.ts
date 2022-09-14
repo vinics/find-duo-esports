4c83b46dadbd1017d67ca3673831e81fd2a1ffdf
vinics <vinics.dr@gmail.com>
2022-09-14 13:06:42 -0300

    incomplete: Day3 (id required on ad create)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,95 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+import { convertHourStringToMinutes } from './utils/convertHourStringToMinutes'
 
 const app = express()
 
-app.get('/ads', () => {
-  console.log('Acessou ads...')
+app.use(express.json())
+
+const prisma = new PrismaClient({
+  log: ['query']
 })
 
-app.listen(3333)
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          Ads: true
+        }
+      }
+    }
+  })
+
+  return response.json(games)
+})
+
+app.post('games/:id/ads', async (request, response) => {
+  const gameId = request.params.id
+  const body = request.body
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel
+    }
+  })
+
+  return response.status(205).json(ad)
+})
+
+app.get('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    }
+  })
+
+  return response.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(',')
+    }
+  }))
+})
+
+app.get('/ads/:id/discord', async (request, response) => {
+  const adId = request.params.id
+
+  const ad =await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId
+    }
+  })
+
+  return response.json({
+    discord: ad.discord,
+  })
+})
+
+const PORT = process.env.PORT || 3333
+
+app.listen(3333, () => console.log(`Server up on port ${PORT}`))
---
+++ server/src/utils/convertHourStringToMinutes.ts
@@ -0,0 +1,6 @@
+export function convertHourStringToMinutes (hourString: string) {
+  const [hours, minutes] = hourString.split(':').map(Number)
+
+  return hours * 60 + minutes 
+}
+
